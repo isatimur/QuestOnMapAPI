c7aea5f91b2546bca287585748d5437707f5c139
Isa Timur <isatimur.it@gmail.com>
2015-09-05 01:37:43 +0300

    updated project structure

--- qom-rest/src/main/java/ru/qom/api/rest/controller/AccountController.java
+++ qom-rest/src/main/java/ru/qom/api/rest/controller/AccountController.java
@@ -1,19 +1,19 @@
-package org.qom.api.controllers;
+package ru.qom.api.rest.controller;
 
-import org.qom.api.dao.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.qom.api.dao.service.AccountDAO;
 
 @RestController
 public class AccountController {
 
     @Autowired
-    AccountRepository accountRepository;
+    AccountDAO accountDAO;
 
     @RequestMapping("/users")
     public ResponseEntity findUsers() {
-        return ResponseEntity.ok().body(accountRepository.findAll());
+        return (ResponseEntity) ResponseEntity.ok().build();
     }
 }
